0496099f2d93db5c1f1b79aa3919bd30464aa180
Jonny Kalambay <jonnyk20@gmail.com>
2019-09-19 03:17:07 +0000

    Explainer diagrams

---
+++ react-ui/src/components/Diagrams/index.jsx
@@ -0,0 +1,2 @@
+export { default as Cycle } from "./Cycle";
+export { default as Touch } from "./Touch";
--- react-ui/src/components/Start/Start.jsx
+++ react-ui/src/components/Start/Start.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import Button from "../Button/Button";
 import { Link } from "react-router-dom";
+import { Cycle, Touch } from "../Diagrams";
 
 import "./Start.scss";
 
 const Start = () => {
   return (
     <div className="start">
-      <div className="start__instructions">Find all the fish</div>
-      <div>This is the video svg</div>
-      <div>These are the instructions for the vide</div>
-      <div>Level up explanation</div>
-      <div>Level up diagram</div>
+      <div className="start__instructions">
+        <p>JellyPhil is an alien jelly fish, who needs to eat and grow</p>
+      </div>
+      <div className="start__cycle">
+        <Cycle />
+      </div>
+      <div className="start__instructions">
+        To help him feed, watch the aquarium, find the fish that he's looking
+        for and touch them on the screen
+      </div>
+      <div className="start__touch">
+        <Touch />
+      </div>
+
       <div>
         <Link to="/play">
           <Button>Start</Button>
